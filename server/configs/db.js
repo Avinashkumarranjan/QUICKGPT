b918fa20c4d09af4edc6b4aa62c6d33b8ba6296d
Avinash kumar <avi977116@gmail.com>
2026-09-22 23:09:43 +0530

    Fix MongoDB database connection

--- server/configs/db.js
+++ server/configs/db.js
@@ -1,11 +1,17 @@
 import mongoose from "mongoose";
 
-const connectDB = async()=>{
+const connectDB = async () => {
     try {
-        mongoose.connection.on("connected",()=>console.log("Database connected"))
-        await mongoose.connect(`${process.env.MONGODB_URI}/quickgpt`)
+        mongoose.connection.on("connected", () => {
+            console.log("Database connected");
+        });
+
+        await mongoose.connect(process.env.MONGODB_URI);
+
     } catch (error) {
-        console.log(error.message,"Error in database")
+        console.log(error.message, "Error in database");
+        throw error;
     }
-}
+};
+
 export default connectDB;
